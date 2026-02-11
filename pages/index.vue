b5dcbbc3d39e47d543af4ea2c9ca2757437e88b3
fofo <fofo@gmail.com>
2026-02-11 17:17:26 +0800

    refactor: 更新首页立即下载按钮样式，调整边框宽度

--- pages/index.vue
+++ pages/index.vue
@@ -242,7 +242,7 @@
               <div class="flex justify-center lg:justify-start">
                 <NuxtLink
                   to="/download-media-kit"
-                  class="learn-more-btn inline-flex items-center justify-center bg-[#F5F5F5] border-2 rounded-[30px] font-pingfang font-normal text-[16px] sm:text-[18px] md:text-[19px] lg:text-[20px] leading-[24px] sm:leading-[27px] md:leading-[28.5px] lg:leading-[30px] tracking-normal transition-colors w-[200px] h-[50px] md:w-[240px] md:h-[60px]"
+                  class="learn-more-btn inline-flex items-center justify-center bg-[#F5F5F5] border-1 rounded-[30px] font-pingfang font-normal text-[16px] sm:text-[18px] md:text-[19px] lg:text-[20px] leading-[24px] sm:leading-[27px] md:leading-[28.5px] lg:leading-[30px] tracking-normal transition-colors w-[200px] h-[50px] md:w-[240px] md:h-[60px]"
                 >
                   立即下载
                 </NuxtLink>
