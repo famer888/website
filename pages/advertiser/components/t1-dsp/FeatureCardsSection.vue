c70318b81dddedc620e6672e587242a3179e847c
fofo <fofo@gmail.com>
2026-02-11 17:13:42 +0800

    refactor: T1DPS 更新 FeatureCardsSection 组件的样式，调整文本宽度和间距，优化响应式布局

--- pages/advertiser/components/t1-dsp/FeatureCardsSection.vue
+++ pages/advertiser/components/t1-dsp/FeatureCardsSection.vue
@@ -1,19 +1,19 @@
 <template>
     <section :class="[bgColor, 'py-14  md:py-[120px]']">
-        <div class="max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <h3
                 class="font-pingfang font-medium tracking-normal text-[#111827] text-center text-[36px] md:text-[56px] leading-[56px] md:leading-[96px] mb-6">
                 {{ title }}
             </h3>
             <p :class="[
-                'font-pingfang font-normal text-left md:text-center text-[16px] md:text-[18px] leading-[26px] md:leading-[26px] tracking-normal text-[#93959C] w-full md:max-w-[600px] mx-auto mb-[49px] md:mb-[90px]',
+                'font-pingfang font-normal text-left md:text-center text-[16px] md:text-[18px] leading-[26px] md:leading-[26px] tracking-normal text-[#93959C] w-full md:max-w-[570px] mx-auto mb-[49px] md:mb-[90px]',
             ]">
                 {{ description }}
             </p>
 
             <!-- PC：三列卡片 -->
             <div :class="[
-                'hidden md:grid grid-cols-3 gap-6',
+                'hidden md:grid grid-cols-3 gap-[40px]',
                 id === 'digital' && 'mb-8',
             ]">
                 <div v-for="item in items" :key="item.title" :class="[
@@ -23,7 +23,7 @@
                         id === 'optimization' ||
                         id === 'browser' ||
                         id === 'digital'
-                        ? 'xl:w-[373px] xl:h-[480px] shadow-[0_2px_24px_0_rgba(0,0,0,0.096)] border-none flex flex-col items-center justify-center mx-auto max-w-[90vw]'
+                        ? 'xl:w-[373px] xl:h-[480px] shadow-[0px_2px_24px_0px_#00000018] border-none flex flex-col items-center justify-center mx-auto max-w-[90vw]'
                         : 'py-28 shadow-[0_5px_8px_-2px_rgba(0,0,0,0.025),0_2px_3px_-1px_rgba(0,0,0,0.01)] border border-[#EEF0F4]',
                 ]">
                     <SvgIcon :name="item.icon"
