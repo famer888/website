8e53d20c4c6169bd11dcfd74dc992bce5834b3c2
fofo <fofo@gmail.com>
2026-03-04 10:52:32 +0800

    fix: 修改文案

--- pages/advertiser/components/how-to-operate/steps-section-2.vue
+++ pages/advertiser/components/how-to-operate/steps-section-2.vue
@@ -5,13 +5,13 @@
 			<div class="pt-[48px] md:pt-[123px] md:pr-[23px] order-1">
 				<h2
 					class="font-pingfang font-medium tracking-[0px] text-[36px] md:text-[64px] leading-[56px] md:leading-[96px] text-[#626671] md:text-[#171A1D] mb-[24px] md:mb-[48px]">
-					上传您的横幅广告</h2>
+					提交素材与规格确认</h2>
 				<h3
 					class="font-pingfang font-medium tracking-[0px] text-[16px] md:text-[24px] leading-[24px] md:leading-[36px] text-[#626671] mb-[16px] md:mb-[24px]">
-					让你的信息表达更有创意！</h3>
+					按投放目标匹配位置，确保上线节奏可控</h3>
 				<p
 					class="font-pingfang font-normal tracking-[0px] text-[14px] md:text-[18px] leading-[20px] md:leading-[28px] text-[#93959C] md:max-w-[560px]">
-					您可以上传横幅图片，也可以从图库中选择之前使用过的图片展示给您的受众。如果您是网站广告新手，不知道该推广什么内容，敬请联系T1 Ads官方，我们将提供各种各样的产品供您选择。</p>
+					从资源列表中选择合适的广告形式与位置，并设置投放节奏与频控。我们会同步确认排期与交付要求，保障稳定上线。</p>
 			</div>
 			<!-- 右侧图片 -->
 			<img src="~/assets/imgaes/advertiser/img_ad3.webp" alt="" class="w-[280px] h-[280px] md:w-[600px] md:h-[600px] object-contain order-2 mx-auto md:mx-0">
